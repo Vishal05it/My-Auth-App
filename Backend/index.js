cd23d86dcfdcbf4a002d5adc431b14f562948e2f
Vishal05it <vishal.tiwari0510@gmail.com>
2026-03-02 15:50:38 +0530

    Origin added

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE"],
-    origin: ["http://localhost:5173", "http://localhost:5174"]
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://my-auth-app-mjis.vercel.app"]
 }))
 app.use(express.json());
 app.use("/user/api", userRouter);
